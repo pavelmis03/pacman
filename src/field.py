9bb9ad60297c5e14fbb50f0d5761422f8c198fce
Sorochan Ilya <super.ilya.66@gmail.com>
2019-11-17 12:06:01 +0300

    Fixed field drawing + changed it appearance

--- src/field.py
+++ src/field.py
@@ -5,12 +5,13 @@ from src.base_classes import DrawableObject
 
 
 class Field(DrawableObject):
-    def __init__(self, game_object, cell_size=20):
+    def __init__(self, game_object, cell_size=20, position=[0, 0]):
         super().__init__(game_object)
         # don't touch cell_size, game level is builded for it and window w & h
         # window width and height must be multiply of cell_size
         # on the original photo 28x30
         self.cell_size = cell_size
+        self.offset = position
         self.cells = [
             "#######################################",
             "# . . . * . . . . ### . . . . * . . . #",
@@ -37,14 +38,15 @@ class Field(DrawableObject):
             "#######################################"
         ]
         self.WALL = '#'
+        self.bump = 2 # выпуклость стен, сделай больше и посмотри что выйдет))
 
     def process_draw(self):
         for i in range(len(self.cells)):
             for j in range(len(self.cells[i])):
-                x = j*self.cell_size
-                y = i*self.cell_size
+                x = self.offset[0] + j*self.cell_size
+                y = self.offset[1] + i*self.cell_size
                 if self.cells[i][j] == self.WALL:
                     color = Color.BLUE
                 else:
                     color = Color.BLACK
-                pygame.draw.rect(self.game_object.screen, color, (x, y, x+self.cell_size, y+self.cell_size))
+                pygame.draw.rect(self.game_object.screen, color, (x, y, self.cell_size, self.cell_size), self.bump)
