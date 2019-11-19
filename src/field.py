07658177e3d0929504fc525fd569cff920549c4b
Sorochan Ilya <super.ilya.66@gmail.com>
2019-11-19 18:35:31 +0300

    Added drawing process of field

--- src/field.py
+++ src/field.py
@@ -1,7 +1,7 @@
 import pygame
 
 from src.constants import Color
-from src.base_classes import DrawableObject
+from src.base_classes import DrawableObject, FieldObject
 
 
 class Field(DrawableObject):
@@ -49,11 +49,19 @@ class Field(DrawableObject):
 
         self.game_objects = []
 
+
+    def get_cell_pos(self, i, j):
+        x = self.offset[0] + j * self.cell_size
+        y = self.offset[1] + i * self.cell_size
+        return [x, y]
+
+    def draw_wall(self, x, y):
+        pygame.draw.rect(self.game_object.screen, Color.DBLUE, (x, y, self.cell_size, self.cell_size), self.bump)
+        pygame.draw.rect(self.game_object.screen, Color.BLUE, (x + self.bump, y + self.bump, self.cell_size - self.bump, self.cell_size - self.bump))
+
     def process_draw(self):
         for i in range(len(self.cells)):
             for j in range(len(self.cells[i])):
-                x = self.offset[0] + j*self.cell_size
-                y = self.offset[1] + i*self.cell_size
+                x, y = self.get_cell_pos(i, j)
                 if self.cells[i][j] == self.WALL:
-                    pygame.draw.rect(self.game_object.screen, Color.DBLUE, (x, y, self.cell_size, self.cell_size), self.bump)
-                    pygame.draw.rect(self.game_object.screen, Color.BLUE, (x+self.bump, y+self.bump, self.cell_size-self.bump, self.cell_size-self.bump))
+                    self.draw_wall(x, y)
