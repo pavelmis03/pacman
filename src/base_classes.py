78d8c1a8f652a8cd15d7dc415ffa16a28668fb43
Sorochan Ilya <super.ilya.66@gmail.com>
2019-11-19 18:51:23 +0300

    Added FieldObject class

--- src/base_classes.py
+++ src/base_classes.py
@@ -11,3 +11,17 @@ class DrawableObject:
 
     def process_draw(self):
         pass
+
+
+class FieldObject(DrawableObject):
+    def __init__(self, game_object, ch, x=0, y=0):
+        self.super().__init__(game_object)
+        # x, y позиция в таблице
+        self.index = ch
+        self.x, self. y = x, y
+
+    def pos(self):
+        return self.game_object.field.get_cell_pos(self.x, self.y)
+
+    def remove_self(self):
+        self.game_object.field.cells[self.x][self.y] = ' '
