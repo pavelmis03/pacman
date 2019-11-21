1f6602f6eebf3df59f8334f9d67a086804dc75e4
Shcheglov Max <makcimy2002@mail.ru>
2019-11-21 03:56:05 +0300

    made a simple move

--- src/acters.py
+++ src/acters.py
@@ -10,7 +10,6 @@ def main():
     # начало -----------------------
     pygame.init()  # иницилизация библиотеки
     screen = pygame.display.set_mode(size)  # создание окна и установка размера
-    screen.fill((0, 0, 0))
     game_over = False #флаг - проверка на выход
     # ------------------------------
 
@@ -19,18 +18,40 @@ def main():
     square.fill((255, 211, 0))
     # ------------------------------
 
-    # главный цикл
+    # разные параметры -------------
+    square_x = 50
+    square_y = 50
+    # ------------------------------
+
+    # главный цикл ------------------------------------------------------
     while not game_over:                        # основной цикл программы
         for event in pygame.event.get():        # получение всех событий
+
             if event.type == pygame.QUIT:       # проверка события выхода
                 game_over = True
 
-        screen.blit(square, (50, 70))  #отобразить объект
+            # реализация движения -------------
+            if event.type == pygame.KEYDOWN:  # проверка нажатия на клавиатуру
+                if event.key == pygame.K_a:
+                    square_x -= 2             # шаг на 2 пикселя влево
+                elif event.key == pygame.K_d:
+                    square_x += 2             # шаг на 2 пикселя вправо
+                elif event.key == pygame.K_w:
+                    square_y -= 2             # шаг на 2 пикселя вверх
+                elif event.key == pygame.K_s:
+                    square_y += 2             # шаг на 2 пикселя вниз
+            # --------------------------------
+
+        # отображение ------------------------
+        screen.fill((0, 0, 0))
+        screen.blit(square, (square_x, square_y))  #отобразить объект
         pygame.display.flip()  # double buffering
         pygame.time.wait(50)  # ждать 50 миллисекунд
+        # ------------------------------------
+
     sys.exit()
 
-    
+
 if __name__ == '__main__':
     main()
 
