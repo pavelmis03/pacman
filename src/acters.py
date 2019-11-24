b2bcd5a333049e3076725e502f8d1eb8b2b075a2
Shcheglov Max <makcimy2002@mail.ru>
2019-11-24 23:42:05 +0300

    workpiece work behavior

--- src/acters.py
+++ src/acters.py
@@ -2,7 +2,6 @@ import pygame
 from random import randrange
 import sys
 
-
 class Bots:
 
     def __init__(self, x, y, color):
@@ -28,10 +27,37 @@ class Bots:
         # ------------------------------------------------------
 
     def process_event(self, event, p_x, p_y):
-        pass
+        self.pacman_x = p_x
+        self.pacman_y = p_y
+        self.ranged = ((self.ghost_in_rect.x - self.pacman_x) ** 2 + (self.ghost_in_rect.y <= self.pacman_y) ** 2) ** 0.5
+
+        self.g_go_left_up = 0
+        self.g_go_left_down = 0
+        self.g_go_right_up = 0
+        self.g_go_right_down = 0
+
+        self.g_go_left = 0
+        self.g_go_right = 0
+        self.g_go_up = 0
+        self.g_go_down = 0
+
+        if self.ranged < 200:
+            self.state = True
+        else:
+            self.state = False
+
+        if self.state:
+            if self.ghost_in_rect.x > self.pacman_x and self.ghost_in_rect.x > self.pacman_x:
+                self.g_go_left_up = 1
+            elif self.ghost_in_rect.x < self.pacman_x and self.ghost_in_rect.x > self.pacman_x:
+                self.g_go_right_up = 1
+            elif self.ghost_in_rect.x > self.pacman_x and self.ghost_in_rect.x < self.pacman_x:
+                self.g_go_left_down = 1
+            elif self.ghost_in_rect.x < self.pacman_x and self.ghost_in_rect.x < self.pacman_x:
+                self.g_go_right_down = 1
 
     def smooth_move(self):
-        self.count_of_steps = randrange(1, 4) #
+        self.count_of_steps = randrange(1, 4)
         self.random_direction = randrange(1, 5)  # рандомное направление
                                                  # 1 -> влево
                                                  # 2 -> вправо
@@ -51,13 +77,13 @@ class Bots:
                     self.ghost_in_rect.x += 5  # шаг
             i = 0
 
-        if self.random_direction == 3:  #  вверх
+        if self.random_direction == 3:  # вверх
             for i in range(self.count_of_steps):
                 if self.ghost_in_rect.y > 20:  # проверка на выход за пределы экрана
                     self.ghost_in_rect.y -= 5  # шаг
             i = 0
 
-        if self.random_direction == 4:  #  вниз
+        if self.random_direction == 4:  # вниз
             for i in range(self.count_of_steps):
                 if self.ghost_in_rect.y < 580:  # проверка на выход за пределы экрана
                     self.ghost_in_rect.y += 5  # шаг
@@ -75,7 +101,7 @@ class Pacman:
         self.pacman_in_rect.x = x
         self.pacman_in_rect.y = y
 
-    def get_coordinate(self):
+    def get_coordinate(self):  # спец метод для получения координат для класса "боты"
         return self.pacman_in_rect
 
     def process_event(self, event):
@@ -95,6 +121,7 @@ class Pacman:
                 self.key_down = 3  # положене "3"
             elif event.key == pygame.K_s:
                 self.key_down = 4  # положене "4"
+
 
     def smooth_move(self, move_left, move_right, move_up, move_down):
         self.move_left = move_left  # флаг анимации движеия влево
@@ -142,13 +169,12 @@ class Pacman:
                     self.pacman = pygame.image.load("pm-down-small.png")
                 # ------------------------------------------------------
 
-
     def draw_object(self, screen):
         screen.blit(self.pacman, self.pacman_in_rect)  # отобразить объект
 
 
 class Game:
-    def __init__(self, width=800, height=600):
+    def __init__(self, width = 800, height = 600):
         self.width = width
         self.height = height
         self.size = [self.width, self.height]
@@ -157,18 +183,19 @@ class Game:
         self.create_game_objects()
 
         # разные параметры -------------
-        self.move_left = False  # флаг анимации движеия влево
+        self.move_left = False   # флаг анимации движеия влево
         self.move_right = False  # флаг анимации движеия вправо
-        self.move_up = False  # флаг анимации движеия вверх
-        self.move_down = False  # флаг анимации движеия вниз
-                                # есть по две разные картинки на каждое направление (всего 8)
-                                # в цикле значение меняется
-                                # если флаг False, то рисуется 1 изображение
-                                # если флаг True, то рисуется 2 изображени
+        self.move_up = False     # флаг анимации движеия вверх
+        self.move_down = False   # флаг анимации движеия вниз
+                                 # есть по две разные картинки на каждое направление (всего 8)
+                                 # в цикле значение меняется
+                                 # если флаг False, то рисуется 1 изображение
+                                 # если флаг True, то рисуется 2 изображени
 
         self.smooth_ghost_move = 0
         self.pacman_coordinate = []
         # ------------------------------
+
 
     def create_game_objects(self):
         self.A = Pacman(300, 50)
@@ -191,21 +218,22 @@ class Game:
         for event in pygame.event.get():
             # Обработка события выхода--------------------------
             if event.type == pygame.QUIT:
-                self.game_over = True
+                 self.game_over = True
             # --------------------------------------------------
 
             # логика движения ----------------------------------
             self.A.process_event(event)
+            self.pacman_coordinate = self.A.get_coordinate()
+            self.G.process_event(event, self.pacman_coordinate[0], self.pacman_coordinate[1])
             # --------------------------------------------------
 
     def process_logic(self):  # логика объектов
         # действие движение --------------
-        self.move_left = not self.move_left  # изменение значения флага
-        self.move_right = not self.move_right  # изменение значения флага
-        self.move_up = not self.move_up  # изменение значения флага
-        self.move_down = not self.move_down  # изменение значения флага
+        self.move_left = not self.move_left     # изменение значения флага
+        self.move_right = not self.move_right   # изменение значения флага
+        self.move_up = not self.move_up         # изменение значения флага
+        self.move_down = not self.move_down     # изменение значения флага
         self.A.smooth_move(self.move_left, self.move_right, self.move_up, self.move_down)
-
         self.G.smooth_move()
         # -------------------------------
 
