73fa1cfa24e4a9e928d32f4d9d47bcc594606163
Osipov Dmitry <osipovdmitry19012015@gmail.com>
2019-11-25 01:16:53 +0300

    updated_food

--- src/food.py
+++ src/food.py
@@ -3,10 +3,11 @@ from src.base_classes import DrawableObject
 from src.constants import Color
 
 class Food(DrawableObject):
-    def __init__(self, game_object, x, y):
+    def __init__(self, game_object, x, y, type):
         super().__init__(game_object)
         self.x = x
         self.y = y
+        self.type = type
 
     def draw_food(self):
         pygame.draw.circle(self.game_object.screen, Color.YELLOW, (self.x, self.y), 3)
@@ -14,6 +15,10 @@ class Food(DrawableObject):
     def draw_superfood(self):
         pygame.draw.circle(self.game_object.screen, Color.YELLOW, (self.x, self.y), 7)
 
-
     def process_draw(self):
-        self.draw_superfood()
+        if self.type == 0:
+            self.draw_food()
+            pass
+        else:
+            self.draw_superfood()
+            pass
