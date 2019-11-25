cc6353be86e94b213528df1b263673059ac24f11
Mochalin Arseniy <coddeboy@gmail.com>
2019-11-25 15:20:37 +0300

    Experiments with design

--- src/constants.py
+++ src/constants.py
@@ -14,42 +14,44 @@ SCREEN_WIDTH = SCREEN_SIZE[0]
 SCREEN_HEIGHT = SCREEN_SIZE[1]
 SCREEN_CENTER = (SCREEN_SIZE[0] // 2, SCREEN_SIZE[1] // 2)
 
-IMAGES_DIR = 'images'
-SOUNDS_DIR = 'sounds'
+IMAGES_DIR = 'images/'
+SOUNDS_DIR = 'sounds/'
+MENU_DIR = 'menu/'
 
 # User Interface(UI)
 BG_COLOR = [20, 20, 20]
 TEXT_COLOR = [30, 250, 250]
-TITLE_SIZE = 35
+TITLE_SIZE = 30
 LABEL_SIZE = 20
 TOOLTIP_SIZE = 15
+MENU_ITEM_SPACE = 10
 
 UI_BTN_NORM_CLR = [250, 250, 250]
-UI_BTN_ACT_CLR = [250, 180, 0]
+UI_BTN_ACT_CLR = [250, 70, 150]
 UI_BTN_SIZE = (200, 30)
 
-FONT_PATH = 'menu/menu_font.ttf'
-PATH_HIGHSCORES = 'menu/highscores.ini'
-PATH_CREDITS = 'menu/credits.ini'
-PATH_CONTROLS = 'menu/controls.ini'
+FONT_PATH = MENU_DIR + 'menu_font.ttf'
+PATH_HIGHSCORES = MENU_DIR + 'highscores.ini'
+PATH_CREDITS = MENU_DIR + 'credits.ini'
+PATH_CONTROLS = MENU_DIR + 'controls.ini'
 
-PATH_LOGO = IMAGES_DIR + '/logo.png'
+PATH_LOGO = IMAGES_DIR + 'logo.png'
 
 RESPONSE = 5 # ms
 
 # Sound mixer constants
 DEBUG_MIXER = True
 SOUNDLIB = {
-    'SOUND_START': SOUNDS_DIR + '/pacman_beginning.wav',
-    'SOUND_CHOMP': SOUNDS_DIR + '/pacman_chomp.wav',
-    'SOUND_DEATH': SOUNDS_DIR + '/pacman_death.wav',
-    'SOUND_EAT_FRUIT': SOUNDS_DIR + '/pacman_eatfruit.wav',
-    'SOUND_EAT_GHOST': SOUNDS_DIR + '/pacman_eatghost.wav',
-    'SOUND_EAT_FINAL': SOUNDS_DIR + '/pacman_intermission.wav',
+    'SOUND_START': SOUNDS_DIR + 'pacman_beginning.wav',
+    'SOUND_CHOMP': SOUNDS_DIR + 'pacman_chomp.wav',
+    'SOUND_DEATH': SOUNDS_DIR + 'pacman_death.wav',
+    'SOUND_EAT_FRUIT': SOUNDS_DIR + 'pacman_eatfruit.wav',
+    'SOUND_EAT_GHOST': SOUNDS_DIR + 'pacman_eatghost.wav',
+    'SOUND_EAT_FINAL': SOUNDS_DIR + 'pacman_intermission.wav',
 }
 # HUD TEST
 SCORES_HUD_FONT_SIZE = 30
-PATH_HEART_IMG = IMAGES_DIR + '/life.png'
+PATH_HEART_IMG = IMAGES_DIR + 'life.png'
 PACMAN_MAX_LIVES = 3
 
 FIELD_MAP = [
