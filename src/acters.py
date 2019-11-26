53919d5473f8abc55278e4b2749c3f8cafdf9023
Shcheglov Max <makcimy2002@mail.ru>
2019-11-26 20:32:44 +0300

    new animation logic

--- src/acters.py
+++ src/acters.py
@@ -96,10 +96,14 @@ class Bots:
 class Pacman:
 
     def __init__(self, x, y):
-        self.pacman = pygame.image.load("pm-right-small.png")
+        self.pacman = pygame.image.load("pm.png")
         self.pacman_in_rect = self.pacman.get_rect()
         self.pacman_in_rect.x = x
         self.pacman_in_rect.y = y
+
+        self.move_animation = 0  # Это флаг анимации. В цикле он постоянно меняется, растет на 1.
+                                 # Теперь можно проверять его кратность тому или иному числу, в зависимости от того,
+                                 # сколько картинок в анимации.
 
     def get_coordinate(self):  # спец метод для получения координат для класса "боты"
         return self.pacman_in_rect
@@ -114,7 +118,7 @@ class Pacman:
 
         if event.type == pygame.KEYDOWN:  # проверка нажатия на клавиатуру
             if event.key == pygame.K_a:
-                self.key_down = 1  # положене "1
+                self.key_down = 1  # положене "1"
             elif event.key == pygame.K_d:
                 self.key_down = 2  # положене "2"
             elif event.key == pygame.K_w:
@@ -123,17 +127,14 @@ class Pacman:
                 self.key_down = 4  # положене "4"
 
 
-    def smooth_move(self, move_left, move_right, move_up, move_down):
-        self.move_left = move_left  # флаг анимации движеия влево
-        self.move_right = move_right  # флаг анимации движеия вправо
-        self.move_up = move_up  # флаг анимации движеия вверх
-        self.move_down = move_down  # флаг анимации движеия вниз
+    def smooth_move(self):
+        self.move_animation += 1
 
         if self.key_down == 1:
             if self.pacman_in_rect.x > 3:  # проверка на выход за пределы экрана
                 self.pacman_in_rect.x -= 3  # шаг влево
                 # Смена спрайта : анимация------------------------------
-                if self.move_left:
+                if self.move_animation % 2 == 0:
                     self.pacman = pygame.image.load("pm-left-big.png")
                 else:
                     self.pacman = pygame.image.load("pm-left-small.png")
@@ -143,7 +144,7 @@ class Pacman:
             if self.pacman_in_rect.x < 797:  # проверка на выход за пределы экрана
                 self.pacman_in_rect.x += 3  # шаг вправо
                 # Смена спрайта : анимация------------------------------
-                if self.move_right:
+                if self.move_animation % 2 == 0:
                     self.pacman = pygame.image.load("pm-right-big.png")
                 else:
                     self.pacman = pygame.image.load("pm-right-small.png")
@@ -153,7 +154,7 @@ class Pacman:
             if self.pacman_in_rect.y > 3:  # проверка на выход за пределы экрана
                 self.pacman_in_rect.y -= 3  # шаг вверх
                 # Смена спрайта : анимация------------------------------
-                if self.move_up:
+                if self.move_animation % 2 == 0:
                     self.pacman = pygame.image.load("pm-up-big.png")
                 else:
                     self.pacman = pygame.image.load("pm-up-small.png")
@@ -163,7 +164,7 @@ class Pacman:
             if self.pacman_in_rect.y < 597:  # проверка на выход за пределы экрана
                 self.pacman_in_rect.y += 3  # шаг вниз
                 # Смена спрайта : анимация------------------------------
-                if self.move_down:
+                if self.move_animation % 2 == 0:
                     self.pacman = pygame.image.load("pm-down-big.png")
                 else:
                     self.pacman = pygame.image.load("pm-down-small.png")
@@ -178,20 +179,12 @@ class Game:
         self.width = width
         self.height = height
         self.size = [self.width, self.height]
+
         self.library_init()
         self.game_over = False
         self.create_game_objects()
 
         # разные параметры -------------
-        self.move_left = False   # флаг анимации движеия влево
-        self.move_right = False  # флаг анимации движеия вправо
-        self.move_up = False     # флаг анимации движеия вверх
-        self.move_down = False   # флаг анимации движеия вниз
-                                 # есть по две разные картинки на каждое направление (всего 8)
-                                 # в цикле значение меняется
-                                 # если флаг False, то рисуется 1 изображение
-                                 # если флаг True, то рисуется 2 изображени
-
         self.smooth_ghost_move = 0
         self.pacman_coordinate = []
         # ------------------------------
@@ -223,17 +216,13 @@ class Game:
 
             # логика движения ----------------------------------
             self.A.process_event(event)
-            self.pacman_coordinate = self.A.get_coordinate()
+            self.pacman_coordinate = self.A.get_coordinate()  # дает координаты пакмана для призрака
             self.G.process_event(event, self.pacman_coordinate[0], self.pacman_coordinate[1])
             # --------------------------------------------------
 
     def process_logic(self):  # логика объектов
         # действие движение --------------
-        self.move_left = not self.move_left     # изменение значения флага
-        self.move_right = not self.move_right   # изменение значения флага
-        self.move_up = not self.move_up         # изменение значения флага
-        self.move_down = not self.move_down     # изменение значения флага
-        self.A.smooth_move(self.move_left, self.move_right, self.move_up, self.move_down)
+        self.A.smooth_move()
         self.G.smooth_move()
         # -------------------------------
 
